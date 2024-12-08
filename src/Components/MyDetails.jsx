96f73611825cd7205579fb9ba9773d00a1c1c33e
Mainul Hossain <138763613+Mainul21@users.noreply.github.com>
2024-12-09 03:03:35 +0600

    complete my Details

--- src/Components/MyDetails.jsx
+++ src/Components/MyDetails.jsx
@@ -1,8 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import {  useRef } from "react";
 import { IoManSharp } from "react-icons/io5";
 
 const MyDetails = () => {
+
+  const textref = useRef(null)
+  const imgRef = useRef(null)
+    useGSAP(()=>{
+      gsap.from(textref.current,{
+        y:-50,
+        opacity:0,
+        duration: 2,
+        delay: 2
+      })
+      gsap.to(imgRef.current,{
+        rotateY: 360,
+        duration: 2,
+        delay: 2
+      })
+    })
+
+
   return (
-    <div className="p-10 ">
+    <div className="p-10 border-b">
       <h1 className="text-5xl font-bold text-center items-center justify-center flex">
         <IoManSharp /> About <span className="text-amber-500"> Me</span>
       </h1>
@@ -10,16 +31,18 @@ const MyDetails = () => {
         <div className="p-10">
           <img
             className="rounded-xl"
+            ref={imgRef}
             height={250}
             width={300}
             src="/src/assets/images/Mainul.jpg"
             alt=""
           />
         </div>
-        <div className="p-10">
-          <h1>Hi! I am Mainul</h1>
-          <h1>Computer Science Student | Web Developer</h1>
-          <p>Brac</p>
+        <div ref={textref} className="p-10 top-">
+          <h1 className="font-bold text-4xl mb-3">Hi! I am Mainul</h1>
+          <h1 className="text-2xl mb-5">Computer Science Student | Web Developer</h1>
+          <p className="text-xl mb-3 text-justify">I am a Computer Science Student Pursuing Bachelors degree from BRAC University. I am interested in <span className="text-white hover:text-blue-500">web development</span>, <span className="text-white hover:text-blue-500">machine learning</span> and <span className="text-white hover:text-blue-500">competitive Programming</span>. My main programming language is <span className="text-white hover:text-blue-500">Python</span>. I also learned basic <span className="text-white hover:text-blue-500">Kotlin</span> and <span className="text-white hover:text-blue-500">C programming</span> for university courses although I am most comfortable with Python. I use <span className="text-white hover:text-blue-500">MERN</span> stack for web devlopment. </p>
+          <h1 className="text-xl text-amber-500 hover:text-blue-500">Location: Dhaka, Bangladesh</h1>
         </div>
       </div>
     </div>
