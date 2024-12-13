b67654659ac0e2f30f6336ba40b4960a619a4672
Mainul Hossain <138763613+Mainul21@users.noreply.github.com>
2024-12-13 16:12:33 +0600

    All done

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,9 +1,37 @@
+import { useRef } from "react";
 import { FaHeadset } from "react-icons/fa6";
-import Typewriter from 'react-ts-typewriter';
+import Typewriter from "react-ts-typewriter";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
+
 
 const ContactMe = () => {
+  const formRef = useRef(null);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log("submitted");
 
-  
+    emailjs
+      .sendForm("service_y1qz4b5", "template_3tqb5ul", formRef.current, {
+        publicKey: "A_7olKZ7ELvFjSpOk",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          Swal.fire({
+            position: "center-center",
+            icon: "success",
+            title: "Email Sent!! Will get back to you",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          formRef.current.reset()
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   return (
     <div className="p-10">
@@ -15,11 +43,17 @@ const ContactMe = () => {
       </div>
       <div className="p-5">
         <form
+          ref={formRef}
+          onSubmit={sendEmail}
           action=""
           className=" border rounded-xl shadow-xl shadow-emerald-500 p-4 grid grid-cols-2"
         >
           <div className="font-bold text-5xl flex items-center ml-3">
-            <Typewriter text='Thank you for getting in touch' loop delay={1000}></Typewriter>
+            <Typewriter
+              text="Thank you for getting in touch"
+              loop
+              delay={1000}
+            ></Typewriter>
           </div>
           <div className="form-control items-end space-y-3">
             <input
@@ -27,23 +61,34 @@ const ContactMe = () => {
               placeholder="Name"
               name="name"
               className="input input-bordered input-success w-full max-w-lg"
+              required
             />
             <input
               type="text"
               placeholder="Email Address"
               name="email"
               className="input input-bordered input-success w-full max-w-lg"
+              required
             />
             <input
               type="text"
               placeholder="Phone Number"
+              name="number"
               className="input input-bordered input-success w-full max-w-lg"
             />
             <textarea
               className="textarea textarea-success w-full max-w-lg"
               placeholder="Message"
               name="message"
+              required
             ></textarea>
+
+            <button
+              value="submit"
+              className="btn bg-amber-500 text-black w-full max-w-lg hover:bg-blue-500 hover:text-white "
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
